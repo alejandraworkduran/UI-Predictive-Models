488c6b48fb52a0b48f94be8e13072c4558f86fa2
Your Name <duran.alejandra.work@gmail.com>
2024-05-27 01:26:49 -0400

    Changes to be committed: 	modified:   app.py 	modified:   best_model.pkl 	modified:   random_forest_model.pkl

--- app.py
+++ app.py
@@ -1,32 +1,25 @@
 import streamlit as st
-import pickle
-from joblib import load
+import pandas as pd
 import datetime
 import geocoder
-import pandas as pd
-import numpy as np
-import xgboost as xgb
+import joblib
 from sklearn.preprocessing import LabelEncoder
+import xgboost as xgb
+import numpy as np
 
 # Load the models
-model = load('best_model.pkl')
-modelxgb = load('xgb_model.pkl')
+model = joblib.load('best_model.pkl')
+modelxgb = joblib.load('xgb_model.pkl')
 
-# Define the Streamlit app
 def main():
     st.image("C:/Users/Ale/OneDrive/Desktop/CAPSTONE/tbD.png", width=200)
     st.title('Predictive Models')
 
-    # Initialize a LabelEncoder
     le = LabelEncoder()
-
-    # Load the species data from output.csv
     species_data = pd.read_csv('C:/Users/Ale/OneDrive/Desktop/CAPSTONE/output.csv')
     le.fit(species_data['rank_species'])
 
-    # Add input options for user
     col1, col2, col3, col4, col5 = st.columns(5)
-    
     with col3:
         current_date = datetime.date.today()
         g = geocoder.ip('me')
@@ -42,10 +35,22 @@ def main():
         month = st.number_input('Month', value=current_date.month)
         day = st.number_input('Day', value=current_date.day)
         if st.button('Will Jellyfish and Humans be Present?'):
-            input_data = [[lat, lon, year, month, day]]
-            st.session_state['dual_presence_prediction'] = model.predict(input_data)[0]
-            st.session_state['lat'] = lat
-            st.session_state['lon'] = lon
+            input_data = pd.DataFrame({'lat': [lat], 'lon': [lon], 'year': [year], 'month': [month], 'day': [day]})
+            
+            try:
+                prediction = model.predict(input_data)
+                st.write('Prediction:', prediction)
+                if prediction[0] == 1:
+                    st.write('Yes, both jellyfish and human presence is expected at this location.')
+                    data_merged = pd.read_csv('C:/Users/Ale/OneDrive/Desktop/CAPSTONE/data_merged_human_jellyfish.csv')
+                    location_data = data_merged[(abs(data_merged['lat'] - lat) <= 0.1) & (abs(data_merged['lon'] - lon) <= 0.1)]
+                    if not location_data.empty:
+                        st.write('Historical Data:')
+                        st.write(location_data)
+                else:
+                    st.write('No, either jellyfish or human presence is not expected at this location.')
+            except Exception as e:
+                st.write("Error making prediction:", e)
 
     with col4:
         unique_species = species_data['rank_species'].unique()
@@ -53,31 +58,12 @@ def main():
         rank_species = st.selectbox('Select Species', options)
         if st.button('Predict Biovolume for Jellyfish'):
             if rank_species == '':
-                st.session_state['biovolume_prediction'] = 'Please select a species.'
+                st.write('Please select a species.')
             else:
                 encoded_species = le.transform([rank_species])
                 input_data = xgb.DMatrix([[year, month, lat, lon, encoded_species[0], day]], feature_names=['year', 'month', 'lat', 'lon', 'rank_species', 'day'])
-                st.session_state['biovolume_prediction'] = modelxgb.predict(input_data)[0]
-
-    # Display results outside of columns
-    if 'dual_presence_prediction' in st.session_state:
-        prediction = st.session_state['dual_presence_prediction']
-        lat = st.session_state['lat']
-        lon = st.session_state['lon']
-        st.write('Dual Presence:', prediction)
-        if prediction == 1:
-            st.write('Yes, both jellyfish and human presence is expected at this location.')
-        else:
-            st.write('No, either jellyfish or human presence is not expected at this location.')
-
-        data_merged = pd.read_csv('C:/Users/Ale/OneDrive/Desktop/CAPSTONE/data_merged_human_jellyfish.csv')
-        location_data = data_merged[(abs(data_merged['lat'] - lat) <= 0.1) & (abs(data_merged['lon'] - lon) <= 0.1)]
-        if not location_data.empty:
-            st.write('Historical Data:')
-            st.write(location_data)
-
-    if 'biovolume_prediction' in st.session_state:
-        st.write('Biovolume Prediction in mm3:', st.session_state['biovolume_prediction'])
+                biovolume_prediction = modelxgb.predict(input_data)
+                st.write('Biovolume Prediction in mm3:', biovolume_prediction)
 
 if __name__ == '__main__':
     main()
