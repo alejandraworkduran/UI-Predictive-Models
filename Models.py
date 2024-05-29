3e6e366b18422b44ebc8b06f78f15774c282f148
Your Name <duran.alejandra.work@gmail.com>
2024-05-29 10:55:12 -0400

    Changes to be committed: 	modified:   Models.py 	modified:   best_model.pkl 	modified:   random_forest_model.pkl

--- Models.py
+++ Models.py
@@ -34,31 +34,18 @@ from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.model_selection import RandomizedSearchCV
 from sklearn.metrics import accuracy_score, classification_report
 from scipy.stats import randint
+from imblearn.over_sampling import SMOTE
 
-# default_data_path= '/content/drive/MyDrive/JeDI.csv'
 default_data_path = "C:\\Users\\Ale\\OneDrive\\Desktop\\CAPSTONE\\JeDI.csv"
+data = pd.read_csv(default_data_path)
 
-data= pd.read_csv(default_data_path)
+print(data.info())
 
-
-
-data.info()
-
-data.head()
-
-# Assuming your data is in a DataFrame called data_dropped_columns
-distinct_count = data['rank_species'].nunique()
-print("Distinct count:", distinct_count)
+print(data.head())
 
 data_dropped_columns = data.drop(columns=['weight_wet','weight_dry','project_title', 'sub_project_title', 'owner_dataset', 'contact', 'date', 'study_type','location_name', 'accompanying_ancillary_data','catch_per_effort','taxon','rank_phylum','rank_genus','rank_class','rank_order','rank_family','data_type','collection_method','depth_upper','depth_lower','density_integrated','biovolume_integrated'])
 
 data_dropped_columns.head()
-
-# # Display distinct values of the 'time_local' column
-# distinct_times = data_dropped_columns['rank_species'].unique()
-
-# # Print the distinct values
-# print(distinct_times)
 
 # Replace 'nd' with 'Unknown' in the 'rank_species' column
 data_dropped_columns['rank_species'] = data_dropped_columns['rank_species'].replace('nd', 'Unknown')
@@ -66,8 +53,7 @@ data_dropped_columns['rank_species'] = data_dropped_columns['rank_species'].repl
 # Convert the predictor column to categorical data type (if not already)
 data_dropped_columns['presence_absence'] = data_dropped_columns['presence_absence'].astype('category')
 
-
-data_dropped_columns.head()
+print(data_dropped_columns.head())
 
 edited_columns = ['net_opening', 'net_mesh', 'depth', 'count_actual', 'density', 'biovolume']
 
@@ -85,14 +71,6 @@ for col in edited_columns:
     # Replace missing values with the calculated mean for the current column
     data_dropped_columns[col].fillna(mean_value, inplace=True)
 
-
-data_dropped_columns.head()
-
-# Display distinct values of the 'time_local' column
-distinct_times = data_dropped_columns['time_local'].unique()
-
-# Print the distinct values
-print(distinct_times)
 
 # Columns to process
 date_columns = ['year', 'month', 'day']
@@ -117,10 +95,7 @@ median_time = pd.to_timedelta(data_dropped_columns['time_local']).median()
 data_dropped_columns['time_local'].fillna(median_time, inplace=True)
 
 # Now, the 'time_local' column should have missing values imputed with the median time.
-
 data_dropped_columns['time_local'] = data_dropped_columns['time_local'].apply(lambda x: str(x).split(' ')[-1])
-
-data_dropped_columns.head()
 
 #convert data types for use in later models
 
@@ -140,11 +115,6 @@ data_dropped_columns['time_seconds'] = data_dropped_columns['time_local'].dt.hou
 
 #  'time_local' is the column you want to drop since we created new column time_seconds
 data_dropped_columns.drop(columns=['time_local'], inplace=True)
-
-
-data_dropped_columns.head()
-
-data_dropped_columns.info()
 
 # # Insert to output file
 # output_file_path = "C:\\Users\\Ale\\OneDrive\\Desktop\\CAPSTONE\\output.csv"
@@ -186,17 +156,26 @@ for index, species_name in enumerate(species_names):
 
 # **Presence Prediction**
 
-# Species Presence Prediction
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, classification_report
+from imblearn.over_sampling import SMOTE
+
+# Assuming data_encoded is your DataFrame with the encoded data
 
 # Split the data into training and testing sets
 X = data_encoded.drop('presence_absence', axis=1)
 y = data_encoded['presence_absence']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
+# Apply SMOTE to balance the data
+smote = SMOTE(random_state=42)
+X_train_smote, y_train_smote = smote.fit_resample(X_train, y_train)
 
-# Train a logistic regression model
+# Train a logistic regression model on the balanced data
 model = LogisticRegression(max_iter=1000)
-model.fit(X_train, y_train)
+model.fit(X_train_smote, y_train_smote)
 
 # Predict on the test set
 y_pred = model.predict(X_test)
@@ -209,15 +188,16 @@ print(f'Accuracy: {accuracy}')
 print('Presence LR Classification Report:')
 print(report)
 
+
 from sklearn.ensemble import RandomForestClassifier
 # tried a different model
 # random forest classifier
 
 # Create a random forest classifier
-model = RandomForestClassifier()
+model = RandomForestClassifier(random_state=42)
 
-# Train the model on the training data
-model.fit(X_train, y_train)
+# Train the model on the balanced training data
+model.fit(X_train_smote, y_train_smote)
 
 # Predict on the test set
 y_pred = model.predict(X_test)
@@ -230,128 +210,19 @@ print(f'Accuracy: {accuracy}')
 print('Presence RF Classification Report:')
 print(report)
 
-# **Biovolume Estimation**
 
+          
 ## **Biovolume Estimation**
 
-
-# # Split the data into training and testing sets
-# X2 = data_encoded.drop('biovolume', axis=1)
-# y2 = data_encoded['biovolume']
-# X_train, X_test, y_train, y_test = train_test_split(X2, y2, test_size=0.2, random_state=42)
-
-# # Rename the variable that overwrites the r2_score function
-# my_r2_score = r2_score
-
-# # Train the model
-# model = LinearRegression()
-# model.fit(X_train, y_train)
-
-# # Predict on the test set
-# y_pred = model.predict(X_test)
-
-# # Evaluate the model
-# print("Biovolume Linear Regression Model")
-# r2_score_ = my_r2_score(y_test, y_pred)
-# print(f'R2 Score: {r2_score_}')
-
-# mse = mean_squared_error(y_test, y_pred)
-# print(f'Mean squared error: {mse}')
-
-# #TRY TO IMPROVE MODEL
-
-# number_of_features = X_train.shape[1]
-
-# print(f"Number of features: {number_of_features}")
-
-#feature importance
-#so we can limit model
-
 import statsmodels.api as sm
-
-# # Fit a linear regression model to the data
-# model = sm.OLS(y_train, X_train).fit()
-
-# # Extract the coefficients from the model
-# coefficients = model.params
-
-# # Calculate the R^2 for each feature
-# r_squared_values = {}
-# for feature in X_train.columns:
-#     coefficient = coefficients[feature]
-#     standard_error = model.bse[feature]
-#     r_squared = (coefficient**2) / (coefficient**2 + standard_error**2)
-#     r_squared_values[feature] = r_squared
-
-# # Rank the features in order of importance
-# ranked_features = sorted(r_squared_values, key=r_squared_values.get, reverse=True)
-
-# # Print the ranked features
-# for feature in ranked_features:
-#     print(f"{feature}: {r_squared_values[feature]}")
-
-# # Split the data into training and testing sets
-# X2 = data_encoded.drop('biovolume', axis=1)
-# y2 = data_encoded['biovolume']
-# X_train, X_test, y_train, y_test = train_test_split(X2, y2, test_size=0.2, random_state=42)
-
-# # Rename the variable that overwrites the r2_score function
-# my_r2_score = r2_score
-
-# # Train the model
-# model = LinearRegression()
-# model.fit(X_train, y_train)
-
-# # Predict on the test set
-# y_pred = model.predict(X_test)
-
-# print("Biovolume Linear Regression Model Limited Features???")
-
-# # Evaluate the model
-# r2_score_ = my_r2_score(y_test, y_pred)
-# print(f'R2 Score: {r2_score_}')
-
-# mse = mean_squared_error(y_test, y_pred)
-# print(f'Mean squared error: {mse}')
-
-#TRY TO IMPROVE MODEL
-
-# number_of_features = X_train.shape[1]
-
-# print(f"Number of features: {number_of_features}")
-
-# #feature importance
-# #so we can limit model
-
-# import statsmodels.api as sm
-
-# # Fit a linear regression model to the data
-# model = sm.OLS(y_train, X_train).fit()
-
-# # Extract the coefficients from the model
-# coefficients = model.params
-
-# # Calculate the R^2 for each feature
-# r_squared_values = {}
-# for feature in X_train.columns:
-#     coefficient = coefficients[feature]
-#     standard_error = model.bse[feature]
-#     r_squared = (coefficient**2) / (coefficient**2 + standard_error**2)
-#     r_squared_values[feature] = r_squared
-
-# # Rank the features in order of importance
-# ranked_features = sorted(r_squared_values, key=r_squared_values.get, reverse=True)
-
-# # Print the ranked features
-# for feature in ranked_features:
-#     print(f"{feature}: {r_squared_values[feature]}")
-
-#New model using new selected 'imporatant' features
-
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
+import xgboost as xgb
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 
+#New model using new selected 'imporatant' features
 data_encoded2 = data_encoded.copy()
 
 # Convert the biovolume column to a binary variable
@@ -363,11 +234,6 @@ X_important = data_encoded2[['year', 'month', 'lat', 'lon', 'rank_species', 'day
 y = data_encoded2[['biovolume']]
 
 X_train, X_test, y_train, y_test = train_test_split(X_important, y, test_size=0.20, random_state=42)
-
-
-import xgboost as xgb
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
 
 # Create DMatrix for XGBoost
 dtrain = xgb.DMatrix(X_train, label=y_train, enable_categorical=True)
@@ -389,6 +255,10 @@ y_pred = bst.predict(dtest)
 # Evaluate the model
 mse = mean_squared_error(y_test, y_pred)
 print("Biovolume XGB Model")
+
+
+
+print("XGB Model Exploration:")
 # Find the maximum and minimum predictions for biovolume
 max_prediction = max(y_pred)
 print(max_prediction)
@@ -403,79 +273,11 @@ print(max_data_point)
 print("Minimum Prediction:")
 print(min_data_point)
 
-# Optional: Save model
-# bst.save_model('xgb_model.json')
-
-# Optional: Load model
-# bst = xgb.Booster()
-# bst.load_model('xgb_model.json')
-
-# # **SPECIES CLASSIFICATION**
-
-# # SPECIES CLASSIFICATION
-
-# import pandas as pd
-# from sklearn.model_selection import train_test_split
-# from sklearn.preprocessing import StandardScaler
-# from sklearn.ensemble import RandomForestClassifier
-# from sklearn.metrics import classification_report, accuracy_score
-
-# # Separating features and target variable
-# X = data_encoded.drop(columns='rank_species')
-# y = data_encoded['rank_species']
-
-# # Normalizing numerical features
-# scaler = StandardScaler()
-# X_scaled = scaler.fit_transform(X)
-
-# # Splitting the data into training and testing sets
-# X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42)
-
-# X_train.shape, X_test.shape, y_train.shape, y_test.shape
-
-# # Training the Random Forest classifier
-# rf_classifier = RandomForestClassifier(random_state=1)
-# rf_classifier.fit(X_train, y_train)
-
-# # Predicting on the test set
-# y_pred = rf_classifier.predict(X_test)
-
-# # Evaluating the model
-# accuracy = accuracy_score(y_test, y_pred)
-# classification_rep = classification_report(y_test, y_pred)
-
-# print(accuracy)
-# print( classification_rep)
-
-#need new model
-#try to improve accuracy
-
-# from xgboost import XGBClassifier
-
-# # Training the XGBoost classifier
-
-# xgb_classifier = XGBClassifier(random_state=1)
-# xgb_classifier.fit(X_train, y_train)
-
-# # Predicting on the test set
-# y_pred = xgb_classifier.predict(X_test)
-
-# # Evaluating the model
-# accuracy = accuracy_score(y_test, y_pred)
-# classification_rep = classification_report(y_test, y_pred)
-
-# print(f'Accuracy: {accuracy}')
-# # print('Classification Report:')
-# # print(classification_rep)
-
-
-
 # BRING IN HUMAN DATA**
 
-# default_data_path= '/content/drive/MyDrive/JeDI.csv'
-default_data_path2 = r"C:\Users\Ale\OneDrive\Desktop\CAPSTONE\data_encoded_human.csv"
+# default_data_path2 = r"C:\Users\Ale\OneDrive\Desktop\CAPSTONE\data_encoded_human.csv"
 
-data_human= pd.read_csv(default_data_path2)
+data_human= pd.read_csv('data_encoded_human.csv')
 
 data_human.info()
 
@@ -563,7 +365,7 @@ from sklearn.metrics import accuracy_score, classification_report
 from scipy.stats import randint
 
 # Load your data (update the path as needed)
-data = pd.read_csv('C:/Users/Ale/OneDrive/Desktop/CAPSTONE/data_merged_human_jellyfish.csv')
+data = pd.read_csv('data_merged_human_jellyfish.csv')
 
 # Ensure feature names match
 X = data[['lat', 'lon', 'year', 'month', 'day']]
@@ -571,6 +373,10 @@ y = data['both_present']
 
 # Split data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Apply SMOTE to the training data
+smote = SMOTE(random_state=42)
+X_train_smote, y_train_smote = smote.fit_resample(X_train, y_train)
 
 # Define the Gradient Boosting classifier and hyperparameter search space
 param_dist = {
@@ -586,7 +392,7 @@ gradient_boosting = GradientBoostingClassifier()
 
 # Perform RandomizedSearchCV
 random_search = RandomizedSearchCV(estimator=gradient_boosting, param_distributions=param_dist, n_iter=10, cv=2, scoring='accuracy', n_jobs=-1)
-random_search.fit(X_train, y_train)
+random_search.fit(X_train_smote, y_train_smote)
 
 # Get best model and hyperparameters
 best_model = random_search.best_estimator_
@@ -595,8 +401,9 @@ best_params = random_search.best_params_
 # Evaluate the best model on the test data
 y_pred = best_model.predict(X_test)
 accuracy = accuracy_score(y_test, y_pred)
-print("Best hyperparameters:", best_params)
-print("Gradient Boosting Test accuracy:", accuracy)
+# print("Best hyperparameters:", best_params)
+print("Presence Gradient Boosting Test accuracy:", accuracy)
+
 print("Classification Report:\n", classification_report(y_test, y_pred))
 
 # Filter the test data where the model predicts 0
